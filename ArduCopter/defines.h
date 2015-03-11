c7d1e6b9e1095cc97d678a95e9d5418674756159
Robert Lefebvre <robert.lefebvre@gmail.com>
2015-03-10 22:10:02 -0400

    Copter: Initial creation of motor_interlock

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -65,6 +65,7 @@ enum aux_sw_func {
     AUXSW_RETRACT_MOUNT,           // Retract Mount
     AUXSW_RELAY,                   // Relay pin on/off (only supports first relay)
     AUXSW_LANDING_GEAR,            // Landing gear controller
+    AUXSW_MOTOR_INTERLOCK,         // Motor On/Off switch
 };
 
 // Frame types
